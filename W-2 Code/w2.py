98c2cb5d17d74884939fcf644d093b7acd43864e
Jay <jay.sanjay.karia@gmail.com>
2021-12-04 20:50:50 +0530

    Update w2.py

--- W-2 Code/w2.py
+++ W-2 Code/w2.py
@@ -24,3 +24,22 @@ allocatedTips = data['response']['forms'][2]['raw_fields']['allocatedTips']['val
 box9 = data['response']['forms'][2]['raw_fields']['box9']['value']
 dependentCareBenefits = data['response']['forms'][2]['raw_fields']['dependentCareBenefits']['value']
 # e (row) remaining
+employeeLastName = data['response']['forms'][2]['raw_fields']['employeeName']['value'].split(' ')[2]
+employeeName = data['response']['forms'][2]['raw_fields']['employeeName']['value'].split(' ')[0]+' '+ data['response']['forms'][2]['raw_fields']['employeeName']['value'].split(' ')[1]
+box12aAmount = data['response']['forms'][2]['raw_fields']['box12aAmount']['value']
+box12bAmount = data['response']['forms'][2]['raw_fields']['box12bAmount']['value']
+box12cAmount = data['response']['forms'][2]['raw_fields']['box12cAmount']['value']
+box12dAmount = data['response']['forms'][2]['raw_fields']['box12dAmount']['value']
+employeeAddressLine1 = data['response']['forms'][2]['raw_fields']['employeeAddressLine1']['value']
+employeeAddressLine2 = data['response']['forms'][2]['raw_fields']['employeeAddressLine2']['value']
+employeeAddressCity = data['response']['forms'][2]['raw_fields']['employeeAddressCity']['value']
+employeeAddressState = data['response']['forms'][2]['raw_fields']['employeeAddressState']['value']
+employeeAddressZip = data['response']['forms'][2]['raw_fields']['employeeAddressZip']['value']
+box14Other = data['response']['forms'][2]['raw_fields']['box14Other']['value']
+year = data['response']['forms'][2]['raw_fields']['year']['value']
+# Done :up
+
+# Writing into a new pdf file
+with open('test.pdf', 'w') as pdf_file:
+    reader = PyPDF2.PdfFileReader(pdf_file)
+    writer = PyPDF2.PdfFileWriter()
