8e5ada63272a0ffe277ffcfbf1679b5036f2d0ed
Jay <jay.sanjay.karia@gmail.com>
2022-01-02 19:17:05 +0530

    end balance fully done

--- Bank Statements/Bank_Gen.py
+++ Bank Statements/Bank_Gen.py
@@ -1,8 +1,15 @@
 # Importing required modules
 import io
+from reportlab.pdfgen.canvas import Canvas
+from pdfrw import PdfReader
+from pdfrw.toreportlab import makerl
+from pdfrw.buildxobj import pagexobj
+from pdfrw import PdfReader
+from pdfrw.toreportlab import makerl
+from pdfrw.buildxobj import pagexobj
 import textwrap
+from PyPDF2.pdf import PdfFileWriter
 import pdfrw
-from reportlab.pdfgen import canvas
 from PyPDF2 import PdfFileReader # getting the page numbers
 import json # getting the data
 import datetime # date range
@@ -62,7 +69,7 @@ keys = ['account_number', 'account_holders', 'holder_address_1', 'holder_address
 pypdf2_reader = PdfFileReader(Input_PDF_file)
 num_pages = pypdf2_reader.getNumPages()
 # String_Input_date = sys.argv[1]
-String_Input_date = '01012022'
+String_Input_date = '01022022'
 
 # getting the start_date and end_date
 if String_Input_date.startswith("01"):
@@ -90,13 +97,6 @@ for periods in data['bank_accounts'][pk]['periods']:
 
 Account_Details[8] = format(float(Account_Details[8]), ".2f")
 Account_Details[9] = format(float(Account_Details[9]), ".2f")
-
-
-holder_info = f"{Account_Details[1][0]}\n{Account_Details[2]}\n{Account_Details[3]}\n{Account_Details[4]} {Account_Details[5]} {Account_Details[6]}"
-if Account_Details[2] == None:
-    holder_info = f"{Account_Details[1][0]}\n{Account_Details[3]}\n{Account_Details[4]} {Account_Details[5]} {Account_Details[6]}"
-elif Account_Details[3] == None:
-    holder_info = f"{Account_Details[1][0]}\n{Account_Details[2]}\n{Account_Details[4]} {Account_Details[5]} {Account_Details[6]}"
 
 #  Calculate running total 
     #  Calculate running total 
@@ -139,89 +139,70 @@ for index in data['txns']:
     td = td.replace('/'+td_year, '')
     adj_txn_dates.append(td)
 
-new_dict = {}
-for index in range(0, len(data['txns'])):
-    new_dict[f"date_{index}"] = adj_txn_dates[index]
-    new_dict[f"amount_{index}"] = amounts[index]
-    new_dict[f"description_{index}"] = descriptions[index]
-    new_dict[f"running_total_{index}"] = running_total[index]
-    
-    # Write txn details to PDF
-    # fillpdfs.write_fillable_pdf(Input_PDF_file, Output_PDF_file, new_dict) 
-        # fillpdfs.write_fillable_pdf(Input_PDF_file, Output_PDF_file, new_dict) 
-    # fillpdfs.write_fillable_pdf(Input_PDF_file, Output_PDF_file, new_dict) 
-
-
-# populating the text fields (holder)
-dict_ = {
-    'period_input': statement_range,
-    'account_number': Account_Details[0],
-
-    'holder_info': holder_info,
-
-    'begin_balance': Account_Details[8],
-    'end_balance': Account_Details[9],
-    'deposits': sum_positive,
-    'withdrawals': sum_negative,
-    'account_type': Account_Details[7],
-}
-
-# Write holder details to PDF
-# fillpdfs.write_fillable_pdf(Output_PDF_file, Output_PDF_file, dict_) #holder details
-
 total_txns = len(data['txns'])
 counter = 0
 
 # X-Y Writing
-y = 160
 date_x = 35
 
+y = 160
+new_page_date_y = 665
+
+txn_per_pages = 30
+end_balance_y = 0
+
 def print_txn_details():
+    page_number = 1
     # Print first page
-    canvas_data = populate_txn_details(0, 7, date_x, y)
-    form = merge(canvas_data, template_path=Input_PDF_file)
-    # Print txn detail pages
-    # if total_txns>7:
-    #     for i in range(8, 28):
-    #         canvas_data = populate_txn_details(8, 28, date_x, y)
-    #         form = merge(canvas_data, template_path=Input_PDF_file)
-
+    canvas_data = populate_txn_details(0, 6, date_x, y, False, page_number)
+    form = merge(canvas_data, template_path=Output_PDF_file)
     save(form, filename=Output_PDF_file)
-    # save(form_1, filename=Output_PDF_file)
+    # Print txn detail pages
+    start_txn_no = 7
+    if total_txns>7:
+        for i in range(start_txn_no, total_txns, txn_per_pages):
+            page_number+=1
+            canvas_data = populate_txn_details(i, i+txn_per_pages-1, date_x, new_page_date_y, True, page_number)
+            form = merge(canvas_data, template_path=Output_PDF_file)
+            save(form, filename=Output_PDF_file)
 
-def populate_txn_details(txn_index_start, txn_index_end, start_x:int, start_y:int) -> io.BytesIO:
+def populate_txn_details(txn_index_start, txn_index_end, start_x:int, start_y:int, new_page:bool, page_number:int) -> io.BytesIO:
     global description
+    global end_balance_y
     global counter
     data = io.BytesIO()
-    pdf = canvas.Canvas(data)
-    j = 0
-    for i in range(txn_index_start, txn_index_end):
-        try:
-            if len(descriptions[i])>45:
-                descriptions[i] =  textwrap.shorten(descriptions[i], width=45)
-
-            if descriptions[0] == descriptions[i] and adj_txn_dates[0] == adj_txn_dates[i] and amounts[0] == amounts[i] and running_total[0] == running_total[i]:
-                pdf.drawString(x=start_x, y=start_y, text=adj_txn_dates[i])
-                pdf.drawString(x=start_x+65, y=start_y, text=descriptions[i])
-                pdf.drawString(x=start_x+395, y=start_y, text="{:,.2f}".format(float(amounts[i])))
-                pdf.drawString(x=start_x+465, y=start_y, text="{:,.2f}".format(float(running_total[i])))
-            else:
-                j+= 20
-                j+=1
-                counter+=1
-                pdf.drawString(x=date_x, y=start_y-j, text=adj_txn_dates[i])
-                pdf.drawString(x=start_x+65, y=start_y-j, text=descriptions[i])
-                pdf.drawString(x=start_x+395, y=start_y-j, text="{:,.2f}".format(float(amounts[i])))
-                pdf.drawString(x=start_x+465, y=start_y-j, text="{:,.2f}".format(float(running_total[i])))
+    pdf = Canvas(data)
+    if new_page is True:
+        counter = 0
+        for i in range(1,page_number):
+            pdf.showPage()
             
+    j = 0
+    for i in range(txn_index_start, txn_index_end + 1):
+        try:
+            max_char_width = 70
+            pdf.setFontSize(8)
+            if len(descriptions[i])>max_char_width:
+                descriptions[i] =  textwrap.shorten(descriptions[i], width=max_char_width)
+            pdf.drawString(x=date_x, y=start_y-j, text=adj_txn_dates[i])
+            # pdf.drawString()
+            pdf.drawString(x=start_x+65, y=start_y-j, text=descriptions[i])
+            pdf.drawRightString(x=start_x+435, y=start_y-j, text="{:,.2f}".format(float(amounts[i])))
+            pdf.drawRightString(x=start_x+505, y=start_y-j, text="{:,.2f}".format(float(running_total[i])))
+            j+= 20
+            # pdf.drawString(x=0, y=start_y+35-g,text="_______________________________________________________________________________________________")
+            counter+=1
         except:
             pass
+    if total_txns<= 7:
+        pdf.setFontSize(10)
+        end_balance_x = date_x
+        end_balance_y = y-(counter*20)
+        pdf.drawString(x=end_balance_x, y=end_balance_y, text="End Balance:")
 
-    g = 0
-    for i in range(0, counter+2):
-        g+= 20
-        g+=1
-        pdf.drawString(x=0, y=start_y+35-g,text="_______________________________________________________________________________________________") 
+        end_balance_value_x = date_x+505
+        end_balance_value_y = end_balance_y
+        pdf.drawRightString(x=end_balance_value_x, y=end_balance_value_y, text="$"+"{:,}".format(float(Account_Details[9])))
 
     pdf.save()
     data.seek(0)
@@ -242,4 +223,85 @@ def save(form: io.BytesIO, filename: str):
     with open(filename, 'wb') as f:
         f.write(form.read())
 
+def insertPages():
+    writer = PdfFileWriter()
+    reader = PdfFileReader(open(Input_PDF_file, 'rb'))
+    writer.insertPage(reader.getPage(0), 0)
+    # writer.insertPage(reader.getPage(1), 1)
+    i = 0
+    for i in range(7, total_txns, txn_per_pages):
+        writer.insertPage(reader.getPage(1), i)
+
+    output = open(Output_PDF_file,'wb')
+    writer.write(output) 
+    output.close()
+
+def insertPageNumbersAndAccountNumberAndHolderDetails():
+    global Account_Details
+    global end_balance_y
+    input_file = Output_PDF_file
+    output_file = Output_PDF_file
+
+    reader = PdfReader(input_file)
+    pages = [pagexobj(p) for p in reader.pages]
+
+    canvas = Canvas(output_file)
+    canvas.setFontSize(10)
+    # statement date range
+    canvas.drawString(x=370, y=740, text=statement_range)
+    # holder info
+    canvas.drawString(x=50, y=600, text=Account_Details[1][0])
+    if f"{Account_Details[2]}" == "None":
+        Account_Details[2] = ""
+        canvas.drawString(x=50, y=585, text=Account_Details[3])
+        canvas.drawString(x=50, y=570, text=Account_Details[4])
+        canvas.drawString(x=105, y=570, text=Account_Details[5])
+        canvas.drawString(x=125, y=570, text=Account_Details[6])
+    elif f"{Account_Details[3]}" == "None":
+        Account_Details[3] = ""
+        canvas.drawString(x=50, y=585, text=Account_Details[2])
+        canvas.drawString(x=50, y=570, text=Account_Details[4])
+        canvas.drawString(x=105, y=570, text=Account_Details[5])
+        canvas.drawString(x=125, y=570, text=Account_Details[6])
+    else:
+        canvas.drawString(x=50, y=585, text=Account_Details[2])
+        canvas.drawString(x=50, y=570, text=Account_Details[3])
+        canvas.drawString(x=50, y=555, text=Account_Details[4])
+        canvas.drawString(x=105, y=555, text=Account_Details[5])
+        canvas.drawString(x=125, y=555, text=Account_Details[6])
+
+    canvas.drawRightString(x=375, y=470, text=Account_Details[8])
+    canvas.drawRightString(x=375, y=410, text=Account_Details[9])
+    canvas.drawRightString(x=375, y=450, text=str(sum_positive))
+    canvas.drawRightString(x=375, y=430, text=str(sum_negative))
+
+    pg_counter = 1
+
+
+    for page_num, page in enumerate(pages, start=1):
+        pg_counter += 1
+        canvas.doForm(makerl(canvas, page))
+
+        footer_text = f"Page {page_num} of {len(pages)}"
+        canvas.saveState()
+        canvas.setStrokeColorRGB(0, 0, 0)
+        canvas.setFont('Times-Roman', 10)
+        canvas.drawString(445, 10, footer_text)
+        canvas.drawString(425, 725, Account_Details[0])
+        canvas.restoreState()
+        canvas.showPage()
+    
+        if pg_counter == len(pages):
+            end_balance_y = new_page_date_y-(20*counter)
+            end_balance_x = 35
+            canvas.drawString(x=end_balance_x, y=end_balance_y, text="End Balance:")
+
+            end_balance_value_x = date_x+505
+            end_balance_value_y = end_balance_y
+            canvas.drawRightString(x=end_balance_value_x, y=end_balance_value_y, text="$"+"{:,}".format(float(Account_Details[9])))
+
+    canvas.save()
+
+insertPages()
 print_txn_details()
+insertPageNumbersAndAccountNumberAndHolderDetails()
