004a0429331e7d9911a81afb09a9b8a8cc9183f4
Jay <jay.sanjay.karia@gmail.com>
2022-01-03 21:42:19 +0530

    more values added...

--- Paystubs/Paystubs_Gen.py
+++ Paystubs/Paystubs_Gen.py
@@ -1,7 +1,17 @@
 # importing required modules
 import json
 
+# global variables
+subtotal_descriptions = []
+subtotal_rates = []
+subtotal_hours = []
+
 def ReadingJSONData():
+
+    global subtotal_descriptions
+    global subtotal_rates
+    global subtotal_hours
+
     with open('./Resources/Paystub_fields.json') as json_file:
         items = json.load(json_file)
     for data in items['response']:
@@ -22,4 +32,16 @@ def ReadingJSONData():
         employee_postal_code = data['employee']['address']['postal_code']
         employee_marital_status = data['employee']['marital_status']
 
+        # earning subtotals
+        for subtotals in data['earnings']['subtotals']:
+
+            temp_description = subtotals['description']
+            temp_rates = subtotals['current_rate']
+            temp_hours = subtotals['current_hours']
+
+            subtotal_descriptions.append(temp_description)
+            subtotal_rates.append(temp_rates)
+            subtotal_hours.append(temp_hours)
+
 ReadingJSONData()
+print(subtotal_hours)
