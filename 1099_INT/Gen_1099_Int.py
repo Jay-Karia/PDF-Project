3aae915f2791cd861ee4a86d856f4ad5c62bafde
Jay <jay.sanjay.karia@gmail.com>
2022-01-12 15:52:05 +0530

    reading json data done

--- 1099_INT/Gen_1099_Int.py
+++ 1099_INT/Gen_1099_Int.py
@@ -8,7 +8,7 @@ def ReadJSONData():
         for item in data['response']['forms']:
             form_type = item['form_type']
             if form_type == 'A_1099_INT':
-                keys = ['a_1099_int-Part3-Boxes(1-8):1-InterestIncome', 'a_1099_int-Part3-Boxes(1-8):2-EarlyWithdrawalPenalty',
+                numeric_data_keys = ['a_1099_int-Part3-Boxes(1-8):1-InterestIncome', 'a_1099_int-Part3-Boxes(1-8):2-EarlyWithdrawalPenalty',
                 'a_1099_int-Part3-Boxes(1-8):3-InterestOnU.S.SavingsBondsAndTreasuryObligations',
                 'a_1099_int-Part3-Boxes(1-8):4-FederalIncomeTaxWithheld',
                 'a_1099_int-Part3-Boxes(1-8):5-InvestmentExpenses',
@@ -24,13 +24,39 @@ def ReadJSONData():
                 'a_1099_int-Part4-Boxes(9-17):16-StateIdentificationNo.',
                 'a_1099_int-Part4-Boxes(9-17):17-StateTaxWithheld']
 
-                values = []
+                payer_and_recipient_data_keys = ['a_1099_int-Part1-PayerPii:payerName', 
+                'a_1099_int-Part2-RecipientPii:recipientAddress:addressLine1',
+                'a_1099_int-Part2-RecipientPii:recipientAddress:addressLine2',
+                'a_1099_int-Part1-PayerPii:payerAddress:city',
+                'a_1099_int-Part1-PayerPii:payerAddress:state',
+                'a_1099_int-Part1-PayerPii:payerAddress:country',
+                'a_1099_int-Part2-RecipientPii:recipientAddress:zip',
+                'a_1099_int-Part1-PayerPii:payerTelephoneNo.',
+                'a_1099_int-Part1-PayerPii:payerTin(FederalIdentificationNumber)',
+                "a_1099_int-Part2-RecipientPii:recipientTin(IdentificationNumber)",
+                'a_1099_int-Part2-RecipientPii:recipientTin(IdentificationNumber)',
+                'a_1099_int-Part2-RecipientPii:recipientName',
+                'a_1099_int-Part2-RecipientPii:recipientAddress:addressLine1',
+                'a_1099_int-Part2-RecipientPii:recipientAddress:addressLine2',
+                'a_1099_int-Part2-RecipientPii:recipientAddress:zip',
+                'a_1099_int-Part2-RecipientPii:recipientAddress:city',
+                'a_1099_int-Part2-RecipientPii:recipientAddress:state',
+                'a_1099_int-Part2-RecipientPii:recipientAddress:country',
+                'a_1099_int-Part2-RecipientPii:accountNumber','a_1099_int-Part1-PayerPii:payerRtn(Optional)']
 
-                for index in range(0, len(keys)):
-                    v = keys[index] = item['raw_fields'][keys[index]]['value'] 
-                    values.append(v)
+                numeric_data_values = []
 
-                print(values)
+                payer_and_recipient_data_values = []
+
+                for index in range(0, len(numeric_data_keys)):
+                    v = numeric_data_keys[index] = item['raw_fields'][numeric_data_keys[index]]['value'] 
+                    numeric_data_values.append(v)
+
+                for index in range(0, len(payer_and_recipient_data_keys)):
+                    pv = payer_and_recipient_data_keys[index] = item['raw_fields'][payer_and_recipient_data_keys[index]]['value'] 
+                    payer_and_recipient_data_values.append(pv)
+
+                print(payer_and_recipient_data_values)
 
 
 ReadJSONData()
